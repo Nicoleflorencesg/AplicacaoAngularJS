36453503262294713f532aec9a9cd6639f3084b1
Nicole Florence Santos Gonçalves <nicoleflorencesg@gmail.com>
2016-09-15 14:45:12 -0300

    o=aplicação stefanini

---
+++ teste/characters-ctrl.js
@@ -0,0 +1,13 @@
+﻿function CharactersCtrl($scope) {
+    $scope.characters = [
+      { name: 'Nicole Florence', age: '22', gender: 'Feminino' },
+      { name: 'Maria Joaquina', age: '30', gender: 'Feminino' },
+      { name: 'Rômulo Campos', age: '27', gender: 'Masculino' },
+      { name: 'Guilherme Penido', age: '21', gender: 'Masculino' },
+      { name: 'Ezequiel Dias', age: '38', gender: 'Masculino' }
+    ];
+}
+
+angular
+  .module('myApp')
+  .controller('CharactersCtrl', CharactersCtrl);
